3fe72ef46897129ec976af40344511abace76e86
Evan LaHurd <evan.lahurd@gmail.com>
2016-06-09 22:39:38 -0600

    Basic C# console application

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Evan";
+            Console.WriteLine("You entered {0} command-line arguments, {1}", args.Length, name);
+            Console.WriteLine("Hello World!");
+            Console.Write("Your arguments are ");
+            foreach (string s in args)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
