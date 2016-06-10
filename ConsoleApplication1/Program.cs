5c46742749e5e79da22d17b7d606c03920a37d2b
Evan LaHurd <evan.lahurd@gmail.com>
2016-06-09 23:10:27 -0600

    Split examples into methods

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,34 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            string name = "Evan";
-            Console.WriteLine("You entered {0} command-line arguments, {1}", args.Length, name);
+namespace ConsoleApplication1 {
+
+    public class Program {
+
+        public static void Main(String[] args) {
+            writeCommandLineArgs(args);
+
+            writeNumbers();
+
+            writeNumberString();
+        }
+
+        public static void writeCommandLineArgs(String[] args) {
+            String name = "Evan";
+            Console.WriteLine("You entered {0} command-line arguments, {1}.", args.Length, name);
             Console.WriteLine("Hello World!");
-            Console.Write("Your arguments are ");
-            foreach (string s in args)
-            {
-                Console.Write(s + " ");
-            }
+            if (args.Length == 1) {
+                Console.Write("Your argument is {0}", args[0]);
+            } else {
+                Console.Write("Your arguments are ");
+                if (args.Length == 0) {
+                    Console.Write("empty");
+                } else {
+                    foreach (string s in args) {
+                        Console.Write(s + " ");
+                    }
+                }
+            }   
             Console.WriteLine();
-            for (int i = 0; i < 10; i++)
-            {
+        }
+
+        public static void writeNumbers() {
+            for (int i = 0; i < 10; i++) {
                 Console.Write(i);
             }
             Console.WriteLine();
+        }
 
+        public static void writeNumberString() {
             StringBuilder builder = new StringBuilder();
             int appendLength = 10;
-            for (int i = 0; i < appendLength; i++)
-            {
+            for (int i = 0; i < appendLength; i++) {
                 builder.Append(i);
-                if (i < (appendLength - 1))
-                {
+                if (i < (appendLength - 1)) {
                     builder.Append(",");
                 }
             }
